12a0c4ab35cede22aabcd0e03cb4e71c734071c4
Nicolas Liochon <liochon@gmail.com>
2019-10-12 23:30:10 +0200

    cleanup

--- q_learning_random_actions.py
+++ q_learning_random_actions.py
@@ -1,5 +1,4 @@
 import gym
-import gym_pow
 #import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,12 +9,14 @@ def choose_action(Q, state,_epsilon):
     if np.random.uniform(0, 1) <_epsilon:
         return choose_random_action(state), True
     else:
-        # better: would be to remove the invalid actions from the Q[state[0], :]
-        action =  np.argmax(Q[state[0], :])
-        if state[1] >= action:
-            return action, False
-        else:
-            return choose_random_action(state), True
+        # Filter invalid actions
+        bestAction = 0 # do nothing, always valid
+        bestScore = Q[state][0][0]
+        for i in range(1, env.action_space.n):
+            if state[1] >= i and Q[state][0][i] > bestScore:
+                bestAction = i
+                bestScore = Q[state][0][i]
+        return bestAction, False
 
 def choose_random_action(state):
     while True:
@@ -26,14 +27,13 @@ def choose_random_action(state):
 def choose_honest_action():
     return 1
 
-def learn(state, state2, reward, action, Q, lr_rate, gamma):
-    predict = Q[state[0], action]
-    '''if episode % 500 == 0:
-        print("predicted value", predict)'''
-    target = reward + gamma * np.max(Q[state2[0], :])
-    Q[state[0], action] = Q[state[0], action] + lr_rate * (target - predict)
+def learn(cur_state, next_state, reward, action, Q, alpha, gamma):
+    predict = Q[cur_state, action]
+    target = reward + gamma * np.max(Q[next_state])
+    Q[cur_state, action] = Q[cur_state, action] + alpha * (target - predict)
 
 def start(type_of_action, lr_rate, gamma):
+    #Q = np.zeros((env.observation_space.n, env.action_space.n))
     Q = np.zeros((100, env.action_space.n))
 
     average_payouts = []
@@ -51,8 +51,8 @@ def start(type_of_action, lr_rate, gamma):
         if episode < 10:
             epsilon = 0.2
         if episode < 5:
-            epsilon = 0.4
-        if episode % 50 == 0:
+            epsilon = 0.04
+        if episode % 2 == 0:
             epsilon = 0
         if episode % 55 == 0:
             epsilon = 0.1
@@ -60,28 +60,28 @@ def start(type_of_action, lr_rate, gamma):
         state = env.reset()
         done = False
         total_payout = 0
-        max_steps=0
+        staps = 0
         lastPrintBlock = 0
         while done is False:
-            #env.render()
-            max_steps+=1
+            epsilonUsed = epsilon if lastPrintBlock < 9000 else 0
+            staps += 1
             rd = False
-            if(type_of_action=="random"):
+            if type_of_action == "random":
                 action = choose_random_action()
-            elif(type_of_action=="honest"):
+            elif type_of_action == "honest":
                 action = choose_honest_action()
             else:
-                action, rd = choose_action(Q, state,epsilon)
-            
+                action, rd = choose_action(Q, state, epsilonUsed)
+
             state2, last_event, time, myBlocks, reward, lastRewardEth, done, info = env.step(action)
 
-            if epsilon == 0 and max_steps < 200:
+            if epsilon == 0 and staps < 200:
                 rds = ""
                 if rd: rds = "(random)"
                 print("STEP, time", time, "myBlocks", myBlocks, "lastRewardEth",lastRewardEth, "action",action, rds, "state",state, "->", state2)
 
             if myBlocks % 500 == 0 and lastPrintBlock != myBlocks:
-                print("BLOCKS, episode", episode, "time:", time, "myBlocks", myBlocks, "lastRewardEth",lastRewardEth,"epsilon", epsilon, "alpha", lr_rate, "gamma", gamma)
+                print("BLOCKS, episode", episode, "time:", time, "myBlocks", myBlocks, "lastRewardEth",lastRewardEth,"epsilon", epsilonUsed, "alpha", lr_rate, "gamma", gamma)
                 lastPrintBlock = myBlocks
 
             learn(state, state2, reward, action, Q, lr_rate, gamma)
@@ -107,7 +107,7 @@ def start(type_of_action, lr_rate, gamma):
 
 def main():
     for alpha in [0.05, 0.20, 0.30, 0.01]:
-        for gamma in [0.60, 0.70, 0.80, 0.90, 0.99, 0.999, 0.50]:
+        for gamma in [0.9, 0.60, 0.70, 0.8, 0.99, 0.999, 0.50]:
             start("agent", alpha, gamma)
 
 if __name__ == '__main__':
